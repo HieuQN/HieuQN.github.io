9212e9fe0de086e092a963adee0483479fa40659
nguyen084 <nguyenquanghieu1996@gmail.com>
2020-05-22 13:10:12 -0400

    first commit

---
+++ biography/honor.php
@@ -0,0 +1,71 @@
+          <section class="section slim-container p-bottom-0 p-top-0">
+            <div class="container">
+              <h3>Honors &amp; Awards</h3>
+              <p>Mauris sit amet pellentesque ipsum, eget mollis leo. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Integer ac ultrices augue, molestie cursus est.</p>
+              <div class="ol-timeline">
+                <div class="tl-item pub-item with-icon"><span class="item-section">2015</span>
+                  <div class="elem-wrapper"><i class="oli oli-university"></i></div>
+                  <div class="content-wrapper">
+                    <h3 class="title">Nobel Prize</h3>
+                    <div class="description">
+                      <p>Phasellus lectus sem, egestas eget sollicitudin ut, lobortis ac nulla. Quisque nibh diam, mattis pharetra aliquet sed, venenatis quis neque. Pellentesque pellentesque iaculis.Phasellus lectus sem, egestas eget sollicitudin ut, lobortis ac nulla. Quisque nibh diam, mattis pharetra aliquet sed, venenatis quis neque. Pellentesque pellentesque iaculis.</p>
+                    </div>
+                  </div>
+                </div>
+                <div class="tl-item pub-item with-icon"><span class="item-section">2000</span>
+                  <div class="elem-wrapper"><i class="oli oli-literature"></i></div>
+                  <div class="content-wrapper">
+                    <h3 class="title">Nuffield Foundation New Lecturer Award</h3>
+                    <div class="description">
+                      <p>Phasellus lectus sem, egestas eget sollicitudin ut, lobortis ac nulla. Quisque nibh diam, mattis pharetra aliquet sed, venenatis quis neque. Pellentesque pellentesque iaculis.Phasellus lectus sem, egestas eget sollicitudin ut, lobortis ac nulla. Quisque nibh diam, mattis pharetra aliquet sed, venenatis quis neque. Pellentesque pellentesque iaculis.</p>
+                    </div>
+                  </div>
+                </div>
+                <div class="tl-item pub-item with-icon">
+                  <div class="elem-wrapper"><i class="oli oli-literature"></i></div>
+                  <div class="content-wrapper">
+                    <h3 class="title">Invaders And Guardians Award</h3>
+                    <div class="description">
+                      <p>Phasellus lectus sem, egestas eget sollicitudin ut, lobortis ac nulla. Quisque nibh diam, mattis pharetra aliquet sed, venenatis quis neque. Pellentesque pellentesque iaculis.Phasellus lectus sem, egestas eget sollicitudin ut, lobortis ac nulla. Quisque nibh diam, mattis pharetra aliquet sed, venenatis quis neque. Pellentesque pellentesque iaculis.</p>
+                    </div>
+                  </div>
+                </div>
+                <div class="tl-item pub-item with-icon">
+                  <div class="elem-wrapper"><i class="oli oli-university"></i></div>
+                  <div class="content-wrapper">
+                    <h3 class="title">Influence Of The Land Award</h3>
+                    <div class="description">
+                      <p>Phasellus lectus sem, egestas eget sollicitudin ut, lobortis ac nulla. Quisque nibh diam, mattis pharetra aliquet sed, venenatis quis neque. Pellentesque pellentesque iaculis.Phasellus lectus sem, egestas eget sollicitudin ut, lobortis ac nulla. Quisque nibh diam, mattis pharetra aliquet sed, venenatis quis neque. Pellentesque pellentesque iaculis. Phasellus lectus sem, egestas eget sollicitudin ut, lobortis ac nulla. Quisque nibh diam, mattis pharetra aliquet sed, venenatis quis neque. Pellentesque pellentesque iaculis.Phasellus lectus sem, egestas eget sollicitudin ut, lobortis ac nulla. Quisque nibh diam, mattis pharetra aliquet sed, venenatis quis neque. Pellentesque pellentesque iaculis. Phasellus lectus sem, egestas eget sollicitudin ut, lobortis ac nulla. Quisque nibh diam, mattis pharetra aliquet sed, venenatis quis neque. Pellentesque pellentesque iaculis.Phasellus lectus sem, egestas eget sollicitudin ut, lobortis ac nulla. </p>
+                    </div>
+                  </div>
+                </div>
+                <div class="tl-item pub-item with-icon"><span class="item-section">1994</span>
+                  <div class="elem-wrapper"><i class="oli oli-like"></i></div>
+                  <div class="content-wrapper">
+                    <h3 class="title">Descendant Without Duty Award</h3>
+                    <div class="description">
+                      <p>Phasellus lectus sem, egestas eget sollicitudin ut, lobortis ac nulla. Quisque nibh diam, mattis pharetra aliquet sed, venenatis quis neque. Pellentesque pellentesque iaculis.Phasellus lectus sem, egestas eget sollicitudin ut, lobortis ac nulla. Quisque nibh diam, mattis pharetra aliquet sed, venenatis quis neque. Pellentesque pellentesque iaculis.</p>
+                    </div>
+                  </div>
+                </div>
+                <div class="tl-item pub-item with-icon"><span class="item-section">1980</span>
+                  <div class="elem-wrapper"><i class="oli oli-biomass"></i></div>
+                  <div class="content-wrapper">
+                    <h3 class="title">Losing The Future Award</h3>
+                    <div class="description">
+                      <p>Phasellus lectus sem, egestas eget sollicitudin ut, lobortis ac nulla. Quisque nibh diam, mattis pharetra aliquet sed, venenatis quis neque. Pellentesque pellentesque iaculis.Phasellus lectus sem, egestas eget sollicitudin ut, lobortis ac nulla. Quisque nibh diam, mattis pharetra aliquet sed, venenatis quis neque. Pellentesque pellentesque iaculis. Phasellus lectus sem, egestas eget sollicitudin ut, lobortis ac nulla. Quisque nibh diam, mattis pharetra aliquet sed, venenatis quis neque. Pellentesque pellentesque iaculis.Phasellus lectus sem, egestas eget sollicitudin ut, lobortis ac nulla. Quisque nibh diam, mattis pharetra aliquet sed, venenatis quis neque. Pellentesque pellentesque iaculis.</p>
+                    </div>
+                  </div>
+                </div>
+                <div class="tl-item pub-item with-icon"><span class="item-section">1950</span>
+                  <div class="elem-wrapper"><i class="oli oli-university"></i></div>
+                  <div class="content-wrapper">
+                    <h3 class="title">Turtles Of The Sea Award</h3>
+                    <div class="description">
+                      <p>Phasellus lectus sem, egestas eget sollicitudin ut, lobortis ac nulla. Quisque nibh diam, mattis pharetra aliquet sed, venenatis quis neque. Pellentesque pellentesque iaculis.Phasellus lectus sem, egestas eget sollicitudin ut, lobortis ac nulla. Quisque nibh diam, mattis pharetra aliquet sed, venenatis quis neque. Pellentesque pellentesque iaculis.</p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </section>
